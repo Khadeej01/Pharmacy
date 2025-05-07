895aff8d0864b5b602dfcf69bbbf37f1b22f8b62
khadijaDev <khadijaoukhlift@gmail.com>
2025-05-07 15:25:34 +0200

    Product model

---
+++ src/main/java/com/Application/pharmacy/ProductManagerApplication.java
@@ -0,0 +1,4 @@
+package com.Application.pharmacy;
+
+public class ProductManagerApplication {
+}
---
+++ src/main/java/com/Application/pharmacy/controller/ProductController.java
@@ -0,0 +1,40 @@
+package com.Application.pharmacy.controller;
+
+import com.Application.pharmacy.model.Product;
+import com.Application.pharmacy.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
