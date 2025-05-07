58cf1ae3fc51b35742c5584952a40f89b9295045
khadijaDev <khadijaoukhlift@gmail.com>
2025-05-07 15:34:20 +0200

    Prroject structure

--- src/main/java/com/Application/pharmacy/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Application.pharmacy.repository;
-
-
-import com.Application.pharmacy.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
