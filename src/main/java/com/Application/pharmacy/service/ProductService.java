ab4d7c9aad4aa2805279b46c9ed1e92b8ad17d16
khadijaDev <khadijaoukhlift@gmail.com>
2025-05-07 15:34:59 +0200

    Product model

---
+++ src/main/java/com/Application/pharmacy/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.Application.pharmacy.service;
+
+import com.Application.pharmacy.model.Product;
+import com.Application.pharmacy.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Optional<Product> existingProduct = productRepository.findById(id);
+        if (existingProduct.isPresent()) {
+            Product product = existingProduct.get();
+            product.setName(updatedProduct.getName());
+            product.setQuantity(updatedProduct.getQuantity());
+            product.setPrice(updatedProduct.getPrice());
+            product.setDescription(updatedProduct.getDescription());
+            return productRepository.save(product);
+        } else {
+            throw new RuntimeException("Product with ID " + id + " not found");
+        }
+    }
+
+    public void deleteProduct(Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Product with ID " + id + " not found");
+        }
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
